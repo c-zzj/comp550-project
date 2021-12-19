765c1771c549d313fcd0ef4ed5737fd04dc5dfaf
Yingxuan Wang <sabrinawyx328@outlook.com>
2021-12-19 18:39:14 -0500

    feat: added data augmentation

--- data.py
+++ data.py
@@ -7,14 +7,19 @@ import torch
 from torch import Tensor
 import re
 import transformers
+import nltk
 from transformers import AutoModel, BertTokenizerFast
 import random
+from random import shuffle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.preprocessing import LabelEncoder
 from nltk.stem import SnowballStemmer
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+
+nltk.download('wordnet')
+from nltk.corpus import wordnet
 
 
 def preprocess(data: List[Any], pipeline: List[Callable]) -> TensorDataset:
@@ -47,7 +52,7 @@ def transform_raw_data(data: Any, pipeline: List[Callable]) -> List[np.ndarray]:
     return data
 
 
-def split_dataset(data: List[np.ndarray], ratios: Tuple[int] = (8, 1, 1), random_seed: Optional[int] = None)\
+def split_dataset(data: List[np.ndarray], ratios: Tuple[int] = (8, 1, 1), random_seed: Optional[int] = None) \
         -> Tuple[List[np.ndarray], List[np.ndarray], List[np.ndarray]]:
     """
     :param data:
@@ -76,7 +81,7 @@ def split_dataset(data: List[np.ndarray], ratios: Tuple[int] = (8, 1, 1), random
 
 def df_to_text_label(data: pd.DataFrame) -> List[np.ndarray]:
     data = data.to_numpy()
-    return [data[:,1], data[:,2]]
+    return [data[:, 1], data[:, 2]]
 
 
 def transform_label_multilabel(data: List[np.ndarray]) -> List[np.ndarray]:
@@ -97,7 +102,7 @@ def transform_label_multiclass(data: List[np.ndarray]) -> List[np.ndarray]:
     return [text, labels]
 
 
-def GetWordProcessor(type=0) -> Callable[[List[np.ndarray]],List[np.ndarray]]:
+def GetWordProcessor(type=0) -> Callable[[List[np.ndarray]], List[np.ndarray]]:
     '''
     0 - remove stopwords, 1 - stemming, 2 - lemmatization
     :param type:
@@ -105,18 +110,21 @@ def GetWordProcessor(type=0) -> Callable[[List[np.ndarray]],List[np.ndarray]]:
     '''
     if type == 0:
         stop = stopwords.words('english')
+
         def transform(s: str) -> str:
             s = s.split(' ')
             s = [w for w in s if w not in stop]
             return ' '.join(s)
     elif type == 1:
         lemmatizer = WordNetLemmatizer()
+
         def transform(s: str) -> str:
             s = s.split(' ')
             s = [lemmatizer.lemmatize(w) for w in s]
             return ' '.join(s)
     else:
         stemmer = SnowballStemmer('english')
+
         def transform(s: str) -> str:
             s = s.split(' ')
             s = [stemmer.stem(w) for w in s]
@@ -126,7 +134,9 @@ def GetWordProcessor(type=0) -> Callable[[List[np.ndarray]],List[np.ndarray]]:
         text = np.array([transform(s) for s in data[0]])
         labels = data[1]
         return [text, labels]
+
     return processor
+
 
 def clean_text(data: List[np.ndarray]) -> List[np.ndarray]:
     def transform(s: str) -> str:
@@ -172,7 +182,6 @@ def GetCharListConverter(num_chars: int = 800,
     return convert_to_charlist
 
 
-
 _TFIDFVEC = TfidfVectorizer()
 
 
@@ -201,3 +210,177 @@ def to_embedding_bert(text_set: np.ndarray) -> transformers.tokenization_utils_b
         return_token_type_ids=False
     )
     return tokens
+
+
+def get_only_chars(line):
+    clean_line = ""
+
+    line = line.replace("’", "")
+    line = line.replace("'", "")
+    line = line.replace("-", " ")
+    line = line.replace("\t", " ")
+    line = line.replace("\n", " ")
+    line = line.lower()
+
+    for char in line:
+        if char in 'qwertyuiopasdfghjklzxcvbnm ':
+            clean_line += char
+        else:
+            clean_line += ' '
+
+    clean_line = re.sub(' +', ' ', clean_line)  # delete extra spaces
+    if clean_line[0] == ' ':
+        clean_line = clean_line[1:]
+    return clean_line
+
+
+def synonym_replacement(words, n):
+    new_words = words.copy()
+    random_word_list = list(set([word for word in words if word not in stopwords.words('english')]))
+    random.shuffle(random_word_list)
+    num_replaced = 0
+    for random_word in random_word_list:
+        synonyms = get_synonyms(random_word)
+        if len(synonyms) >= 1:
+            synonym = random.choice(list(synonyms))
+            new_words = [synonym if word == random_word else word for word in new_words]
+            num_replaced += 1
+        if num_replaced >= n:  # only replace up to n words
+            break
+
+    sentence = ' '.join(new_words)
+    new_words = sentence.split(' ')
+
+    return new_words
+
+
+def get_synonyms(word):
+    synonyms = set()
+    for syn in wordnet.synsets(word):
+        for l in syn.lemmas():
+            synonym = l.name().replace("_", " ").replace("-", " ").lower()
+            synonym = "".join([char for char in synonym if char in ' qwertyuiopasdfghjklzxcvbnm'])
+            synonyms.add(synonym)
+    if word in synonyms:
+        synonyms.remove(word)
+    return list(synonyms)
+
+
+def random_deletion(words, p):
+    # obviously, if there's only one word, don't delete it
+    if len(words) == 1:
+        return words
+
+    # randomly delete words with probability p
+    new_words = []
+    for word in words:
+        r = random.uniform(0, 1)
+        if r > p:
+            new_words.append(word)
+
+    # if you end up deleting all words, just return a random word
+    if len(new_words) == 0:
+        rand_int = random.randint(0, len(words) - 1)
+        return [words[rand_int]]
+
+    return new_words
+
+
+def random_swap(words, n):
+    new_words = words.copy()
+    for _ in range(n):
+        new_words = swap_word(new_words)
+    return new_words
+
+
+def swap_word(new_words):
+    random_idx_1 = random.randint(0, len(new_words) - 1)
+    random_idx_2 = random_idx_1
+    counter = 0
+    while random_idx_2 == random_idx_1:
+        random_idx_2 = random.randint(0, len(new_words) - 1)
+        counter += 1
+        if counter > 3:
+            return new_words
+    new_words[random_idx_1], new_words[random_idx_2] = new_words[random_idx_2], new_words[random_idx_1]
+    return new_words
+
+
+def random_insertion(words, n):
+    new_words = words.copy()
+    for _ in range(n):
+        add_word(new_words)
+    return new_words
+
+
+def add_word(new_words):
+    synonyms = []
+    counter = 0
+    while len(synonyms) < 1:
+        random_word = new_words[random.randint(0, len(new_words) - 1)]
+        synonyms = get_synonyms(random_word)
+        counter += 1
+        if counter >= 10:
+            return
+    random_synonym = synonyms[0]
+    random_idx = random.randint(0, len(new_words) - 1)
+    new_words.insert(random_idx, random_synonym)
+
+
+def eda(data, alpha_sr, alpha_ri, alpha_rs, p_rd, num_aug):
+    df = data[1].values.tolist()
+    label = data[0].values.tolist()
+    for sentence in df:
+        sentence = get_only_chars(sentence)
+        words = sentence.split(' ')
+        words = [word for word in words if word is not '']
+        num_words = len(words)
+
+        augmented_sentences = []
+        num_new_per_technique = int(num_aug / 4) + 1
+
+        # sr
+        if (alpha_sr > 0):
+            n_sr = max(1, int(alpha_sr * num_words))
+            for _ in range(num_new_per_technique):
+                a_words = synonym_replacement(words, n_sr)
+                augmented_sentences.append(' '.join(a_words))
+
+        # ri
+        if (alpha_ri > 0):
+            n_ri = max(1, int(alpha_ri * num_words))
+            for _ in range(num_new_per_technique):
+                a_words = random_insertion(words, n_ri)
+                augmented_sentences.append(' '.join(a_words))
+
+        # rs
+        if (alpha_rs > 0):
+            n_rs = max(1, int(alpha_rs * num_words))
+            for _ in range(num_new_per_technique):
+                a_words = random_swap(words, n_rs)
+                augmented_sentences.append(' '.join(a_words))
+
+        # rd
+        if (p_rd > 0):
+            for _ in range(num_new_per_technique):
+                a_words = random_deletion(words, p_rd)
+                augmented_sentences.append(' '.join(a_words))
+
+        augmented_sentences = [get_only_chars(sentence) for sentence in augmented_sentences]
+        shuffle(augmented_sentences)
+
+        # trim so that we have the desired number of augmented sentences
+        if num_aug >= 1:
+            augmented_sentences = augmented_sentences[:num_aug]
+        else:
+            keep_prob = num_aug / len(augmented_sentences)
+            augmented_sentences = [s for s in augmented_sentences if random.uniform(0, 1) < keep_prob]
+
+        # append the original sentence
+        for lab in label:
+            for sent in augmented_sentences:
+                to_append = [lab, sent]
+                df_length = len(df)
+                df.loc[df_length] = to_append
+
+    return df
